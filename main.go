7d12c5da6d2c835b90d4d270009f20a22debd14f
Eduardo Henrique <edu.hen.fm@gmail.com>
2023-07-15 12:44:17 -0400

    :label: Add some mock variable
    The variables and comments are related to how Go works

--- main.go
+++ main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+)
+
+// We can declare variables outside of main function using the following syntax
+//
+// var NAME TYPE
+//
+// But we can't use NAME := VALUE
+//
+// This means that the variable can be initialized without a value before the main function,
+// but it can only contain values inside the function itself
+
+func main() {
+	// var card string = "Ace of Spades" is the same as below
+	//
+	// the ":=" infers the type, and initializes the variable with the given value
+	card := "Ace of Spades"
+
+	// Remember that, when changing variable values, there's no need to use ":=", since
+	// the variable is already initialized
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
+}
