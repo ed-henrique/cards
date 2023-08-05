b716318ef94a105057fa9882d01c9c0ebf8cf06b
Eduardo Henrique <edu.hen.fm@gmail.com>
2023-08-05 13:17:13 -0400

    :sparkles: Add shuffle and file IO functions

--- deck.go
+++ deck.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 type deck []string
 
@@ -18,6 +24,19 @@ func newDeck() deck {
 	return cards
 }
 
+func newDeckFromFile(fileName string) deck {
+	bs, err := os.ReadFile(fileName)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+
+	return deck(s)
+}
+
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
@@ -25,5 +44,26 @@ func deal(d deck, handSize int) (deck, deck) {
 func (d deck) print() {
 	for _, card := range d {
 		fmt.Println(card)
+	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(fileName string) error {
+	return os.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+
+func (d deck) shuffle() {
+	s := len(d)
+
+	// Using current time in nanoseconds as a seed to generate random numbers
+	so := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(so)
+
+	for i := 0; i < s; i++ {
+		n := r.Intn(s)
+		d[i], d[n] = d[n], d[i]
 	}
 }
--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 func main() {
 	cards := newDeck()
-	cards.print()
-
+	// cards.print()
 	hand, cards := deal(cards, 5)
-
-	hand.print()
+	// hand.print()
+	// cards.print()
+	hand.saveToFile("test")
+	//cards2 := newDeckFromFile("test")
+	//cards2.print()
+	cards.shuffle()
 	cards.print()
 }
